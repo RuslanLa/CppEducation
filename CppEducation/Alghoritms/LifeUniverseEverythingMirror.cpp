90e149f0a6ca498064040eccb0b353c4028c26e1
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-01-11 17:54:10 +0300

    solved the [TEST] Life, the Universe, and Everything problem from the spoj

---
+++ CppEducation/Alghoritms/LifeUniverseEverythingMirror.cpp
@@ -0,0 +1,17 @@
+﻿#include "LifeUniverseEverythingMirror.h"
+#include <string>
+#include <iostream>
+#ifndef STOP_READ_CONST
+#define STOP_READ_CONST "42"
+#endif
+/*Solution for the http://www.spoj.com/problems/TEST/ */
+void LifeUniverseEverythingMirror::Run()
+{
+	std::string input;
+	std::cin >> input;
+	while (input != STOP_READ_CONST)
+	{
+		std::cout <<  input << std::endl;
+		std::cin >> input;
+	}
+}
---
+++ CppEducation/Alghoritms/LifeUniverseEverythingMirror.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+/*Solution for the http://www.spoj.com/problems/TEST/
+class LifeUniverseEverythingMirror
+{
+public:
+	static void Run();
+	
+};
