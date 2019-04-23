eae2bed0227f9ea737c3b8930c01f14724ca4a5f
ruslan_latypov <ruslan.latypov@akvelon.com>
2019-04-23 17:39:48 +0300

    Added Q06 task description

--- ArraysAndStrings/Q06.cpp
+++ ArraysAndStrings/Q06.cpp
@@ -4,6 +4,11 @@
 #include <iostream>
 using namespace std;
 
+/*
+ * Implement a method to perform basic string compression using the counts of repeated characters. For example,
+ * the string aabcccccaaa would become a2blc5a3. If the "compressed" string would not become smaller than the original string,
+ * your method should return the original string. You can assume the string has only uppercase and lowercase letters (a -z). Hints
+ */
 
 bool tryWriteToResult(string& result, int32_t count, char character, int32_t& position){
     auto countView =  to_string(count);
