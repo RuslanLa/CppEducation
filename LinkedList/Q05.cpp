be9d09242ec2ce6be2cb212eefca4529483b7398
ruslan_latypov <ruslan.latypov@akvelon.com>
2019-04-30 09:54:11 +0300

    Added task description to Q05

--- LinkedList/Q05.cpp
+++ LinkedList/Q05.cpp
@@ -4,7 +4,14 @@
 #include <algorithm>
 #include <iterator>
 #include "LinkedListUtils.h"
-
+/*
+ * Sum Lists: You have two numbers represented by a linked list, where each node contains a single digit.
+ * The digits are stored in reverse order, such that the 1 's digit is at the head of the list.
+ * Write a function that adds the two numbers and returns the sum as a linked list.
+ * EXAMPLE Input: (7-> 1 -> 6) + (5 -> 9 -> 2). 617 + 295. Output: 2 -> 1 -> 9. 912. F
+ * FOLLOW UP Suppose the digits are stored in forward order. Repeat the above problem.
+ * EXAMPLE lnput:(6 -> 1 -> 7) + (2 -> 9 -> 5). 617 + 295. Output: 9 -> 1 -> 2.  912.
+ * */
 
 void insertSum(std::forward_list<int>& result, std::forward_list<int>::iterator& iterator, int first, int second, int& remainder) {
     auto sum = first + second + remainder;
